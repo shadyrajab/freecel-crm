602f23406a4487994a5101a54d353a6f7d9c890b
shady <shadyrajaab@gmail.com>
2024-02-06 13:36:23 -0300

    Atualizando o horário do agendamento

--- index.py
+++ index.py
@@ -3,10 +3,12 @@ from datetime import datetime
 from functions.crm import get_crm_panel
 from database.update_crm import update_crm
 
+print(datetime.now())
+
 async def check():
     while True:
         now = datetime.now()
-        if now.hour == 12 and now.minute == 36:
+        if now.hour == 23 and now.minute == 59:
             print('Função executada')
             dataHoraInicioCarga = f'{now.year}-{now.month}-{now.day} 00:00:00'
             dataHoraFimCarga = f'{now.year}-{now.month}-{now.day} 23:59:00'
