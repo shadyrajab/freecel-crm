b1ef933d4f80fe29a6fd89542912a827bcdb354c
shady <shadyrajaab@gmail.com>
2024-02-06 13:57:01 -0300

    Trocando o fuso horário manualmente

--- index.py
+++ index.py
@@ -2,12 +2,15 @@ import asyncio
 from datetime import datetime
 from functions.crm import get_crm_panel
 from database.update_crm import update_crm
+import pytz
 
-print(datetime.now())
+fuso_horario_brasil = pytz.timezone('America/Sao_Paulo')
+
+print(datetime.now(fuso_horario_brasil))
 
 async def check():
     while True:
-        now = datetime.now()
+        now = datetime.now(fuso_horario_brasil)
         if now.hour == 23 and now.minute == 59:
             print('Função executada')
             dataHoraInicioCarga = f'{now.year}-{now.month}-{now.day} 00:00:00'
